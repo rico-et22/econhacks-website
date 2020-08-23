80af115effb76e9cd9b2cc3c4a5bc863543cd808
Kamil Pawlak <ricoet22@gmail.com>
2020-08-23 22:14:58 +0200

    feat: header background photo

--- components/header.js
+++ components/header.js
@@ -5,29 +5,31 @@ import Link from 'next/link'
 export default function Header(props) {
   return (
     <header className={styles.header}>
-      <Container>
-        <nav className={styles.nav}>
-          <Link href="/">
-            <img src="logo.png" className={styles.logo}></img>
-          </Link>
-          <div className={styles.nav_right}>
-            <a href="#" className={styles.nav_link}>
-              About
-            </a>
-            <a href="#" className={styles.nav_link_cta}>
+      <div className={styles.header_color_bg}>
+        <Container>
+          <nav className={styles.nav}>
+            <Link href="/">
+              <img src="logo.png" className={styles.logo}></img>
+            </Link>
+            <div className={styles.nav_right}>
+              <a href="#" className={styles.nav_link}>
+                About
+              </a>
+              <a href="#" className={styles.nav_link_cta}>
+                Register
+              </a>
+            </div>
+          </nav>
+          <div className={styles.header_content}>
+            <h1 className={styles.header_title}>EconHacks 2020</h1>
+            <p className={styles.header_description}>The first student-led <br/> economics hackathon.</p>
+            <p className={styles.header_date}>Launch Date - End Date</p>
+            <a href="#" className={styles.header_cta_button}>
               Register
             </a>
           </div>
-        </nav>
-        <div className={styles.header_content}>
-          <h1 className={styles.header_title}>EconHacks 2020</h1>
-          <p className={styles.header_description}>The first student-led <br/> economics hackathon.</p>
-          <p className={styles.header_date}>Launch Date - End Date</p>
-          <a href="#" className={styles.header_cta_button}>
-            Register
-          </a>
-        </div>
-      </Container>
+        </Container>
+      </div>
     </header>
   )
 }
