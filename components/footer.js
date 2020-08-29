e65b56039f398a3ba98e979368cb1d741aae3581
Kamil Pawlak <ricoet22@gmail.com>
2020-08-29 12:47:33 +0200

    style: footer redesign - no design/dev credits, everything centered, hack club instagram icon

--- components/footer.js
+++ components/footer.js
@@ -1,44 +1,33 @@
 import Container from './container'
 import styles from '../styles/Footer.module.css'
+import Icon from '@hackclub/icons'
 
 export default function Footer(props) {
   return (
     <footer className={styles.footer}>
       <Container>
         <div className={styles.footer_content}>
-          <div>
-            <p>Photo by&nbsp;
-              <a
-                href="https://unsplash.com/@m_b_m"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                M. B. M.
-              </a>
-            </p>
-          </div>
-          <div>
-            <p>
-              Site designed by&nbsp;
-              <a
-                href="https://kamilpawlak.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Kamil Pawlak
-              </a> & Benjamin Chen
-            </p>
-            <p>
-              Coded by&nbsp;
-              <a
-                href="https://kamilpawlak.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Kamil Pawlak
-              </a>
-            </p>
-          </div>
+          <a
+            href="https://www.instagram.com/econhacks/"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="Our Instagram"
+          >
+            <Icon
+              glyph="instagram"
+              size={48}
+              alt="Instagram"
+            />
+          </a>
+          <p className={styles.footer_content_photoCredits}>Background photo by&nbsp;
+            <a
+              href="https://unsplash.com/@m_b_m"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              M. B. M.
+            </a>
+          </p>
         </div>
       </Container>
     </footer>
