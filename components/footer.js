25752d6f1933b5290907f9c6040272c74a83d2c6
Kamil Pawlak <ricoet22@gmail.com>
2020-08-30 15:43:19 +0200

    feat: Hack+ info in footer

--- components/footer.js
+++ components/footer.js
@@ -19,6 +19,17 @@ export default function Footer(props) {
               alt="Instagram"
             />
           </a>
+          <p className={styles.footer_content_hackplus}>
+            EconHacks 2020 is a fiscally sponsored project of&nbsp;
+            <a
+              href="https://hackplus.io"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+            Hack+.
+            </a>
+            &nbsp;EIN: 81-1543325
+          </p>
           <p className={styles.footer_content_photoCredits}>Background photo by&nbsp;
             <a
               href="https://unsplash.com/@m_b_m"
