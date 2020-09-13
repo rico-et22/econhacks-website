b50180b3450beb29249ca66990bae03b83078c40
Kamil Pawlak <ricoet22@gmail.com>
2020-09-13 21:59:03 +0200

    feat: mobile navbar behavior

--- components/navbar.js
+++ components/navbar.js
@@ -3,10 +3,12 @@ import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 import Container from './container'
+import Icon from '@hackclub/icons'
 
 export default function Navbar(props) {
   const router = useRouter()
   const [isSticky, setIsSticky] = useState(false)
+  const [isMobileListOpen, setIsMobileListOpen] = useState(false)
   const handleScroll=() => {
     const offset=window.scrollY;
     if(offset > 16 ){
@@ -20,29 +22,70 @@ export default function Navbar(props) {
     window.addEventListener('scroll',handleScroll)
   })
   return (
-    <div className={`${styles.nav_wrapper} ${isSticky ? styles.sticky : ''}`}>
+    <div className={`${styles.nav_wrapper} ${isSticky ? styles.sticky : ''} ${isSticky || isMobileListOpen ? styles.hasBackground : ''}`}>
       <Container>
         <nav className={styles.nav}>
           <Link href="/">
             <img src="logo.jpg" className={styles.logo}></img>
           </Link>
           <div className={styles.nav_right}>
-            <Link href="/about">
-              <a className={`${styles.nav_link} ${router.pathname === '/about' ? styles.active : ''}`}>About</a>
-            </Link>
-            <Link href="#">
-              <a className={`${styles.nav_link} ${router.pathname === '/schedule' ? styles.active : ''}`}>Schedule</a>
-            </Link>
-            <Link href="/#faq">
-              <a className={`${styles.nav_link} ${router.pathname === '/#faq' ? styles.active : ''}`}>FAQ</a>
-            </Link>
+            <div className={styles.desktop_only}>
+              <Link href="/about">
+                <a className={`${styles.nav_link} ${router.pathname === '/about' ? styles.active : ''}`}>About</a>
+              </Link>
+              <Link href="#">
+                <a className={`${styles.nav_link} ${router.pathname === '/schedule' ? styles.active : ''}`}>Schedule</a>
+              </Link>
+              <Link href="/#faq">
+                <a className={`${styles.nav_link} ${router.pathname === '/#faq' ? styles.active : ''}`}>FAQ</a>
+              </Link>
+            </div>
             <Link href="/register">
               <a className={`${styles.nav_link_cta} ${router.pathname === '/register' ? styles.active : ''}`}>
                 Register
               </a>
             </Link>
+            <button
+              className={styles.nav_mobile_menu_button}
+              onClick={() => setIsMobileListOpen(!isMobileListOpen)}
+            >
+              <Icon
+                glyph="menu"
+                size={64}
+                fill="#fff"
+                className={styles.nav_mobile_menu_button_icon}
+              />
+            </button>
           </div>
         </nav>
+        <div className={`${styles.nav_mobile_links_wrapper} ${isMobileListOpen ? styles.open : ''}`}>
+        <div className={`${styles.nav_mobile_links} ${isMobileListOpen ? styles.open : ''}`}>
+          <Link href="/about">
+            <a
+              className={`${styles.nav_link} ${router.pathname === '/about' ? styles.active : ''}`}
+              onClick={() => setIsMobileListOpen(!isMobileListOpen)}
+            >
+              About
+            </a>
+          </Link>
+          <Link href="#">
+            <a
+              className={`${styles.nav_link} ${router.pathname === '/schedule' ? styles.active : ''}`}
+              onClick={() => setIsMobileListOpen(!isMobileListOpen)}
+            >
+              Schedule
+            </a>
+          </Link>
+          <Link href="/#faq">
+            <a
+              className={`${styles.nav_link} ${router.pathname === '/#faq' ? styles.active : ''}`}
+              onClick={() => setIsMobileListOpen(!isMobileListOpen)}
+            >
+              FAQ
+            </a>
+          </Link>
+        </div>
+        </div>
       </Container>
     </div>
   )
