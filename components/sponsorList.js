78d2363739f08fbf8722d4a9c9752eae3a73ac3d
Kamil Pawlak <ricoet22@gmail.com>
2020-10-03 22:49:46 +0200

    feat: display sponsor categories only if they contain any sponsors

--- components/sponsorList.js
+++ components/sponsorList.js
@@ -1,32 +1,14 @@
 import styles from '../styles/SponsorList.module.css'
 
 export default function SponsorList(props) {
+  const mainSponsors = props.sponsors.filter(sponsor => sponsor.logoSize !== 'additionalSupport')
+  const additionalSupport = props.sponsors.filter(sponsor => sponsor.logoSize === 'additionalSupport')
+
   return (
     <>
-      <div className={styles.sponsor_list}>
-        {props.sponsors.filter(sponsor => sponsor.logoSize !== 'additionalSupport').map((sponsor, index) => {
-          return (
-            <a
-              href={sponsor.website}
-              target="_blank"
-              rel="noopener noreferrer"
-              className={styles.sponsor_list_item}
-              key={index}
-            >
-              <img
-                src={sponsor.logo.url}
-                alt={sponsor.name}
-                title={sponsor.name}
-                className={styles.sponsor_list_item_logo}
-              />
-            </a>
-          )
-        })}
-      </div>
-      <div>
-        <h3>Additional support</h3>
-        <div className={`${styles.sponsor_list} ${styles.sponsor_list_additional}`}>
-          {props.sponsors.filter(sponsor => sponsor.logoSize === 'additionalSupport').map((sponsor, index) => {
+      { mainSponsors.length > 0 &&
+        <div className={styles.sponsor_list}>
+          {mainSponsors.map((sponsor, index) => {
             return (
               <a
                 href={sponsor.website}
@@ -45,7 +27,32 @@ export default function SponsorList(props) {
             )
           })}
         </div>
-      </div>
+      }
+      { additionalSupport.length > 0 &&
+        <div>
+          <h3>Additional support</h3>
+          <div className={`${styles.sponsor_list} ${styles.sponsor_list_additional}`}>
+            {additionalSupport.map((sponsor, index) => {
+              return (
+                <a
+                  href={sponsor.website}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={styles.sponsor_list_item}
+                  key={index}
+                >
+                  <img
+                    src={sponsor.logo.url}
+                    alt={sponsor.name}
+                    title={sponsor.name}
+                    className={styles.sponsor_list_item_logo}
+                  />
+                </a>
+              )
+            })}
+          </div>
+        </div>
+      }
   </>
   )
 }
