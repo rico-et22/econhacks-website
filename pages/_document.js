5efbb6d66e468c88741c2df328acaff12c3cdc07
Kamil Pawlak <ricoet22@gmail.com>
2021-01-15 10:36:36 +0100

    feat: new version of "Apply with Devfolio" button

--- pages/_document.js
+++ pages/_document.js
@@ -24,6 +24,7 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <script defer async src="https://apply.devfolio.co/v2/sdk.js"></script>
         </body>
       </Html>
     )
