a29eaf9aac140cb9d0d374456d67aabb0cdde51f
Kamil Pawlak <ricoet22@gmail.com>
2021-01-22 22:53:42 +0100

    fix: "Apply with Devfolio" button not appearing after going back to homepage
    This commit brings back previous useEffect script loading approach - but without the script.onload thing due to the script's newer version

--- components/applyButton.js
+++ components/applyButton.js
@@ -1,4 +1,17 @@
+import { useEffect } from 'react'
+
 export default function ApplyButton(props) { 
+  const loadApplyNowScript = () => {
+    const script = document.createElement('script');
+    script.src = 'https://apply.devfolio.co/v2/sdk.js';
+    script.async = true;
+    document.body.appendChild(script);
+  };
+  
+  useEffect(() => {
+    window.onload = loadApplyNowScript()
+  }, [])
+
   return (
     <div 
       className="apply-button" 
--- pages/_document.js
+++ pages/_document.js
@@ -24,7 +24,6 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
-          <script defer async src="https://apply.devfolio.co/v2/sdk.js"></script>
         </body>
       </Html>
     )
