fc2a905c7648f4fd1143db0ece82e2432653a89d
Kamil Pawlak <ricoet22@gmail.com>
2021-01-22 22:14:37 +0100

    feat: EconHacks promo video in header

--- components/header.js
+++ components/header.js
@@ -2,7 +2,6 @@ import styles from '../styles/Header.module.css'
 import Container from './container'
 import { Parallax } from 'react-parallax'
 import ApplyButton from './applyButton'
-import CTAButton from './ctaButton'
 
 export default function Header(props) {
   return (
@@ -16,12 +15,15 @@ export default function Header(props) {
         <Container>
           <div className={styles.navbar_filler}/>
           <div className={styles.header_content}>
-            <h1 className={styles.header_title}>EconHacks 2021</h1>
-            <p className={styles.header_description}>The largest <br/> economics hackathon.</p>
-            <p className={styles.header_date}>February 13 - 14th</p>
-            <div className={styles.header_buttons}>
-              <ApplyButton/>
+            <div>
+              <h1 className={styles.header_title}>EconHacks 2021</h1>
+              <p className={styles.header_description}>The largest <br/> economics hackathon.</p>
+              <p className={styles.header_date}>February 13 - 14th</p>
+              <div className={styles.header_buttons}>
+                <ApplyButton/>
+              </div>
             </div>
+            <iframe src="https://www.youtube.com/embed/rx4CigJ4Tmc" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen className={styles.header_video}></iframe>
           </div>
         </Container>
       </div>
